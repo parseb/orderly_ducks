64e55347831a628ea4452e980112f85dec75a9da
Bogdan Arsene <arenebp@gmail.com>
2021-06-21 12:57:05 +0300

    bf full join

--- config/routes.rb
+++ config/routes.rb
@@ -7,17 +7,8 @@ Rails.application.routes.draw do
 
   get "/users/", to: 'home#landing', method: :get  
 
-  #get '*path', to: 'home#landing' #revise
-
   resources :sessions
 
-
-    # devise_scope :user do 
-  #   root to: 'static_pages#home'
-  #   match '/sessions/user', to: 'devise/sessions#create', via: :post
-  # end
-
   post "/addsession/", to: "sessions#addsession", as: "addsession"
-  #devise_for :installs
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
